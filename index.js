eb98712ad3f0ea97edd891854f4d707070673230
Apoh <Edenra08@gmail.com>
2022-03-14 01:08:43 +0000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+function add (a, b){
+    return a + b;}
+
+
+function subtract(a, b){
+    return a - b;}
+
+function multiply(a, b){ 
+    return a * b;}
+
+function divide(a, b){ 
+    return a/b;
+}
+
+number =1;
+function add5(number) {
+  return (number += 5);
+}
+
+function subtract3(number) {
+  return (number -= 3);}
